e7a2c3a271f0cdc1816dd90cb45f3dba48996ab2
Arfmann <arfmann@protonmail.com>
2021-03-05 13:30:38 +0100

    Added credits for APIs

--- lib/ui/widgets/settings_info/info_section.dart
+++ lib/ui/widgets/settings_info/info_section.dart
@@ -1,5 +1,6 @@
 import 'package:astropocket/backend/global_variables.dart';
 import 'package:astropocket/backend/url_launcher.dart';
+import 'package:astropocket/style/custom_icons/expand_icon_icons.dart';
 import 'package:astropocket/style/custom_icons/github_icon_icons.dart';
 import 'package:astropocket/style/custom_icons/instagram_icon_icons.dart';
 import 'package:flutter/material.dart';
@@ -28,8 +29,8 @@ class _InfoSectionState extends State<InfoSection> {
             child: Card(
               child: Image.asset("assets/images/logo.png"),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(getWidth(context) / 36.0)
-              ),
+                  borderRadius:
+                      BorderRadius.circular(getWidth(context) / 36.0)),
             ),
           ),
         ),
@@ -142,16 +143,186 @@ class _InfoSectionState extends State<InfoSection> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              'Illustrations by Icons8',
+              'Credits',
               style: GoogleFonts.poppins(
                   fontSize: 16.0, fontWeight: FontWeight.w600),
             ),
             IconButton(
-                icon: Icon(Icons.web_outlined),
-                onPressed: () {
-                  launchURL("https://icons8.com/");
-                })
+              icon: Icon(ExpandCustomIcon.arrow_expand),
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(getWidth(context) / 36.0)),
+                    builder: (context) {
+                      return Container(
+                        height: getHeight(context),
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              top: getHeight(context) / 30.0,
+                              left: getWidth(context) / 30.0,
+                              right: getWidth(context) / 30.0),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                      bottom: getHeight(context) / 60.0),
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                            right: getWidth(context) / 30.0),
+                                        child: Icon(Icons.api_outlined),
+                                      ),
+                                      Text(
+                                        'API',
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'News by Spaceflight API',
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    IconButton(
+                                        icon: Icon(Icons.web_outlined),
+                                        onPressed: () {
+                                          launchURL(
+                                              "https://spaceflightnewsapi.net/");
+                                        })
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Reports by Spaceflight API',
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    IconButton(
+                                        icon: Icon(Icons.web_outlined),
+                                        onPressed: () {
+                                          launchURL(
+                                              "https://spaceflightnewsapi.net/");
+                                        })
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Launches by Launch Library 2',
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    IconButton(
+                                        icon: Icon(Icons.web_outlined),
+                                        onPressed: () {
+                                          launchURL(
+                                              "https://thespacedevs.com/llapi");
+                                        })
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Picture of the day by NASA',
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    IconButton(
+                                        icon: Icon(Icons.web_outlined),
+                                        onPressed: () {
+                                          launchURL("https://api.nasa.gov/");
+                                        })
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'InSight Weather by NASA',
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    IconButton(
+                                        icon: Icon(Icons.web_outlined),
+                                        onPressed: () {
+                                          launchURL("https://api.nasa.gov/");
+                                        })
+                                  ],
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                      top: getHeight(context) / 30.0,
+                                      bottom: getHeight(context) / 60.0),
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                            right: getWidth(context) / 36.0),
+                                        child: Icon(Icons.photo_filter_sharp),
+                                      ),
+                                      Text(
+                                        'Resources',
+                                        style: GoogleFonts.poppins(
+                                            fontSize: 22.0,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      'Illustrations by Icons8',
+                                      style: GoogleFonts.poppins(
+                                          fontSize: 16.0,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    IconButton(
+                                        icon: Icon(Icons.web_outlined),
+                                        onPressed: () {
+                                          launchURL("https://icons8.com/");
+                                        })
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    });
+              },
+            )
           ],
+        ),
+        Padding(
+          padding: EdgeInsets.only(bottom: getWidth(context) / 36.0),
         )
       ],
     );

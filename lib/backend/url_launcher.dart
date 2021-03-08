79d7f55a014b2734532dc1bdae29ead294e327f8
Arfmann <arfmann@protonmail.com>
2021-03-08 22:57:06 +0100

    Changed package

--- lib/backend/url_launcher.dart
+++ lib/backend/url_launcher.dart
@@ -1,9 +1,29 @@
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 
-Future<void> launchURL(url) async {
-    if (await canLaunch(url)) {
-      await launch(url);
+void launchURL(url) async {
+    /*if (await canLaunch(url)) {
+      await launch(url, enableDomStorage: true, enableJavaScript: true, forceWebView: true);
     } else {
+      throw 'Could not launch $url';
+    }*/
+
+    try {
+      await launch(
+        url,
+          option: new CustomTabsOption(
+            enableDefaultShare: true,
+          enableUrlBarHiding: true,
+          showPageTitle: true,
+          extraCustomTabs: <String>[
+            // ref. https://play.google.com/store/apps/details?id=org.mozilla.firefox
+            'org.mozilla.firefox',
+            // ref. https://play.google.com/store/apps/details?id=com.microsoft.emmx
+            'com.microsoft.emmx',
+          ],        
+          ),    
+          
+      );
+    } catch (e) {
       throw 'Could not launch $url';
     }
   }

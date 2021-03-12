99a9f14102a02511cbc2e3909891ad05b681a3bd
Arfmann <arfmann@protonmail.com>
2021-03-12 22:09:48 +0100

    Fix

--- lib/ui/screens/changelog.dart
+++ lib/ui/screens/changelog.dart
@@ -13,49 +13,55 @@ class Changelog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: AppBar(
-          title: Material(
-            color: Colors.transparent,
-            child: Text(
-              'What\'s new',
-              style: GoogleFonts.poppins(
-                  fontSize: 21.5, fontWeight: FontWeight.w600),
-            ),
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.close_outlined),
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => HomeDrawerBuilder()),
-                    (route) => false);
-              },
-            )
-          ],
-        ),
-        body: Padding(
-          padding: EdgeInsets.only(
-              top: getHeight(context) / 15.0,
-              left: getWidth(context) / 20.0,
-              right: getWidth(context) / 36.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'v' + versionNumber.toString(),
+    return  MaterialApp(
+       title: 'Astropocket',
+      themeMode: themeChanger.currentTheme(),
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Material(
+              color: Colors.transparent,
+              child: Text(
+                'What\'s new',
                 style: GoogleFonts.poppins(
-                    fontSize: 17.0, fontWeight: FontWeight.w500),
+                    fontSize: 21.5, fontWeight: FontWeight.w600),
               ),
-
-              // New features and Fixes widgets. Just comment if one of them is not present
-             // NewFeatures(),
-              Fixes()
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.close_outlined),
+                onPressed: () {
+                  Navigator.pushAndRemoveUntil(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => HomeDrawerBuilder()),
+                      (route) => true);
+                },
+              )
             ],
           ),
-        ),
+          body: Padding(
+            padding: EdgeInsets.only(
+                top: getHeight(context) / 15.0,
+                left: getWidth(context) / 20.0,
+                right: getWidth(context) / 36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'v' + versionNumber.toString(),
+                  style: GoogleFonts.poppins(
+                      fontSize: 17.0, fontWeight: FontWeight.w500),
+                ),
+
+                // New features and Fixes widgets. Just comment if one of them is not present
+                NewFeatures(),
+                Fixes()
+              ],
+            ),
+          ),
+      ),
     );
   }
 }

91f0849f0459dae1b176f16061e0336a21d84b47
Arfmann <arfmann@protonmail.com>
2021-04-11 14:41:44 +0200

    Added ISS Tracker API

---
+++ lib/backend/api/iss_tracker_api.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class IssLocationApi {
+  final double long;
+  final double lat;
+  final double altitude;
+  final double velocity;
+
+  IssLocationApi({this.long, this.lat, this.altitude, this.velocity});
+
+  factory IssLocationApi.fromJson(Map<String, dynamic> json) {
+    return IssLocationApi(
+        long: json['longitude'],
+        lat: json['latitude'],
+        altitude: json['altitude'],
+        velocity: json['velocity']
+        );
+  }
+}
+
+Future<IssLocationApi> fetchLocation() async {
+  try {
+    final response = await http.get("https://api.wheretheiss.at/v1/satellites/25544");
+
+    if (response.statusCode == 200)
+      return IssLocationApi.fromJson(json.decode(response.body));
+    else
+      throw Exception('Failed to load JSON');
+  } catch (_) {
+    print(_.toString());
+    return null;
+  }
+}
+
+Stream<IssLocationApi> streamLocation() async* {
+  while (true) {
+        await Future.delayed(Duration(seconds: 1));
+    yield await fetchLocation();
+  }
+}

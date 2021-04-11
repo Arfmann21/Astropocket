0b285a1bebd42e5a80ad452b19e30eb8a92e3112
Arfmann <arfmann@protonmail.com>
2021-04-11 14:44:33 +0200

    Added skeleton loading for next passes

---
+++ lib/ui/widgets/isstracker/isspassesskeleton.dart
@@ -0,0 +1,26 @@
+import 'package:astropocket/backend/global_variables.dart';
+import 'package:astropocket/style/specific_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:skeleton_animation/skeleton_animation.dart';
+
+class IssPassesSkeleton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(getWidth(context) / 70.0)
+      ),
+      width: getWidth(context),
+      height: getWidth(context) / 3.1,
+      margin: EdgeInsets.only(
+          left: getWidth(context) / 36.0,
+          right: getWidth(context) / 36.0,
+          top: getWidth(context) / 20.0),
+      child: Skeleton(
+        baseColor: SpecificColors(context).pulseColorBase,
+        // The Highlight color is the same for both white and dark theme
+        hightlightColor: const Color(0xFF053361),
+      ),
+    );
+  }
+}

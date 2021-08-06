0a6551e49078538615d351944f5acaadfe4bee2b
Arfmann <arfmann@protonmail.com>
2021-08-06 17:04:45 +0200

    UI changes

--- lib/ui/widgets/isstracker/isspassesskeleton.dart
+++ lib/ui/widgets/isstracker/isspassesskeleton.dart
@@ -7,19 +7,21 @@ class IssPassesSkeleton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(getWidth(context) / 70.0)
-      ),
-      width: getWidth(context),
-      height: getWidth(context) / 3.1,
       margin: EdgeInsets.only(
           left: getWidth(context) / 36.0,
           right: getWidth(context) / 36.0,
           top: getWidth(context) / 20.0),
-      child: Skeleton(
-        baseColor: SpecificColors(context).pulseColorBase,
-        // The Highlight color is the same for both white and dark theme
-        hightlightColor: const Color(0xFF053361),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(getWidth(context) / 36.0),
+        child: Container(
+          width: getWidth(context),
+          height: getWidth(context) / 3.1,
+          child: Skeleton(
+            baseColor: SpecificColors(context).pulseColorBase,
+            // The Highlight color is the same for both white and dark theme
+            hightlightColor: const Color(0xFF053361),
+          ),
+        ),
       ),
     );
   }

86ea705335b3b082654fd5c838b021ae983faa5a
Arfmann <arfmann@protonmail.com>
2021-08-06 17:05:47 +0200

    UI changes

--- lib/ui/widgets/isstracker/mapskeleton.dart
+++ lib/ui/widgets/isstracker/mapskeleton.dart
@@ -1,5 +1,4 @@
 import 'package:astropocket/backend/global_variables.dart';
-import 'package:astropocket/style/custom_icons/expand_icon_icons.dart';
 import 'package:astropocket/style/specific_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -31,20 +30,16 @@ class IssMapSkeleton extends StatelessWidget {
           SizedBox(
             height: getWidth(context) / 30.0,
           ),
-          Container(
-            height: getHeight(context) / 4,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(getWidth(context) / 36.0),
-            ),
-            margin: EdgeInsets.only(
-                left: getWidth(context) / 36.0,
-                right: getWidth(context) / 36.0,
-                top: getWidth(context) / 20.0),
-            child: Skeleton(
-              baseColor: SpecificColors(context).pulseColorBase,
-              // The Highlight color is the same for both white and dark theme
-              hightlightColor: const Color(0xFF053361),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(getWidth(context) / 36.0),
+            child: Container(
+              height: getHeight(context) / 4,
+              width: MediaQuery.of(context).size.width,
+              child: Skeleton(
+                baseColor: SpecificColors(context).pulseColorBase,
+                // The Highlight color is the same for both white and dark theme
+                hightlightColor: const Color(0xFF053361),
+              ),
             ),
           )
         ],

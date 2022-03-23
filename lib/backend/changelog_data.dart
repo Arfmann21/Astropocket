96ba90dab3e26fc77d12d6477db04699044bba71
Arfmann21 <arfmann@protonmail.com>
2022-03-23 22:28:56 +0100

    Class for dyanmic changelog UI creation

---
+++ lib/backend/changelog_data.dart
@@ -0,0 +1,16 @@
+class ChangelogData {
+  List<String> fixesTitles = [
+    'Spelling: ',
+    'UI: ',
+    'Drawer: ',
+    'Changelog: ',
+    'Optimizations: '
+  ];
+  List<String> fixesContent = [
+    'fixed the wrong text in Mercury\'s fact n.9.',
+    'changed some font weights and colors for a better interface.',
+    'fixed a bug that was preventing the drawer from opening.',
+    'fixed a bug causing dark theme app bar showing up while using light theme.',
+    'various code changes for a better overall experience.'
+  ];
+}
